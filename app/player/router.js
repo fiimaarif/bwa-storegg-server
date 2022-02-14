3e91f266f36bd3ea702e4421a88782b972cbeb10
fiimaarif <fiimaarif44@gmail.com>
2022-02-14 15:27:24 +0700

    update router player

--- app/player/router.js
+++ app/player/router.js
@@ -24,7 +24,7 @@ router.get("/history/:id/detail", isLoginPlayer, historyDetail);
 router.get("/dashboard", isLoginPlayer, dashboard);
 router.get("/profile", isLoginPlayer, profile);
 router.put(
-  "/profile",
+  "/profile/:id",
   isLoginPlayer,
   multer({ dest: os.tmpdir() }).single("image"),
   editProfile
